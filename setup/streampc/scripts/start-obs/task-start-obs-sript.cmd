c1837796bf006e7453377870047e113d334e83d0
Stream PC <KopiasCsaba@users.noreply.github.com>
2024-02-08 15:09:45 +0200

    Update restreamer & fix obs startup

--- setup/streampc/scripts/start-obs/task-start-obs-sript.cmd
+++ setup/streampc/scripts/start-obs/task-start-obs-sript.cmd
@@ -1,2 +1,2 @@
 # This script is used by the sceduled task: scheduled-task-for-start-obs.xml
-start /d "C:\Program Files\obs-studio\bin\64bit\" obs64.exe
+start /d "C:\Program Files\obs-studio\bin\64bit\" obs64.exe  --disable-shutdown-check
