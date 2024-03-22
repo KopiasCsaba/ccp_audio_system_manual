0fd28075206ee89f25b43778d84adc50d8498b6d
Stream PC <KopiasCsaba@users.noreply.github.com>
2024-03-22 13:12:35 +0200

    Add script for enforcing display resolutions, Fix keyboard PTZ controls to be more smoother by using hotkeys of the ptz plugin.

---
+++ setup/streampc/scripts/set-displays/set-displays.cmd
@@ -0,0 +1,5 @@
+﻿# Tool is from https://github.com/RickStrahl/SetResolution
+
+SetResolution.exe SET -noprompt -m1 -w 1280 -h 1024 -f 60
+SetResolution.exe SET -noprompt -m2 -w 1920 -h 1080 -f 60
+SetResolution.exe SET -noprompt -m3 -w 1920 -h 1080 -f 50
