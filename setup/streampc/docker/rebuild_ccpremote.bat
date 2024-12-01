d29716bc1f38695c833cb000b497d0bdae633379
Kopiás Csaba <KopiasCsaba@users.noreply.github.com>
2024-12-01 21:26:13 +0200

    Add remote control

---
+++ setup/streampc/docker/rebuild_ccpremote.bat
@@ -0,0 +1,4 @@
+docker compose down ccpremote
+docker compose build --no-cache ccpremote
+docker compose up -d ccpremote
+
--- setup/streampc/docker/start_all.cmd
+++ setup/streampc/docker/start_all.cmd
@@ -1,2 +1,3 @@
 docker compose up -d restreamer
 docker compose up -d console-obs-bridge
+docker compose up -d ccpremote
